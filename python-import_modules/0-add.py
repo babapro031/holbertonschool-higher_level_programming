696a620a3ebf20e242009cff5e59a98a8d948ae9
babapro031 <stormgainucun1@gmail.com>
2025-11-22 17:26:18 +0400

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as ad0
 if __name__ == "__main__":
   a=1
   b=2
-  result =add_0.add(a,b)
+  result =ad0.add(a,b)
   print("Result: {}".format(result))
+
 
 
 
