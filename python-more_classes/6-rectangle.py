6aadda841bafa165e3f26dedc716457b9f66ce30
babapro031 <stormgainucun1@gmail.com>
2025-11-28 19:43:53 +0400

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
 	"""Represent a rectangle."""
 	
-    number_of_instances = 0
+	number_of_instances = 0
     
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle with optional width and height."""
